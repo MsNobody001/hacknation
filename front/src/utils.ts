5501f068101f3fbb429109543f13d38ed813373a
Denys Savytskyi <deonissavytsk@gmail.com>
2025-12-07 06:37:49 +0100

    fix date formatting

--- front/src/utils.ts
+++ front/src/utils.ts
@@ -5,3 +5,8 @@ export const isValidEmail = (email: string) => {
 export const formatTime = (date: Date) => {
   return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 };
+
+export const convertDate = (dateStr: string) => {
+  const [day, month, year] = dateStr.split('.');
+  return `${year}-${month}-${day}`;
+}
